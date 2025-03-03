3d155dc997cf79721d9cf234ca096d6de9fcd83f
taka4655 <taku.enginner@gmail.com>
2025-03-03 20:15:27 +0900

    fix:fetch時のエラー分岐作成

--- app/controllers/musixmatch_controller.rb
+++ app/controllers/musixmatch_controller.rb
@@ -25,18 +25,22 @@ class MusixmatchController < ApplicationController
   def fetch_track(api_key, q_track, q_artist)
     track_encoded_uri = URI::DEFAULT_PARSER.escape("https://api.musixmatch.com/ws/1.1/matcher.track.get?apikey=#{api_key}&q_track=#{q_track}&q_artist=#{q_artist}")
     track_response = HTTParty.get(track_encoded_uri)
-    return nil if track_response.body.blank?
+    track_response_code_check = JSON.parse(track_response)
+    # puts "track_response: #{JSON.pretty_generate(track_response_code_check)}"
+    return nil if track_response_code_check['message']['header']['status_code'] == 404
 
-    parsed_json_track_response = JSON.parse(track_response.body)
+    parsed_json_track_response = JSON.parse(track_response)
     parsed_json_track_response['message']['body']['track'].slice('track_name', 'artist_name')
   end
 
   def fetch_lyrics(api_key, q_track, q_artist)
     lyrics_encoded_uri = URI::DEFAULT_PARSER.escape("https://api.musixmatch.com/ws/1.1/matcher.lyrics.get?apikey=#{api_key}&q_track=#{q_track}&q_artist=#{q_artist}")
     lyrics_response = HTTParty.get(lyrics_encoded_uri)
-    return nil if lyrics_response.body.blank?
+    lyrics_response_code_check = JSON.parse(lyrics_response)
+    # puts "lyrics_response: #{JSON.pretty_generate(lyrics_response_code_check)}"
+    return nil if lyrics_response_code_check['message']['header']['status_code'] == 404
 
-    parsed_json_lyrics_response = JSON.parse(lyrics_response.body)
+    parsed_json_lyrics_response = JSON.parse(lyrics_response)
     parsed_json_lyrics_response['message']['body']['lyrics']['lyrics_body']
   end
 end
