a104e4217e9486c3d30422a551c4341d017a3836
taka4655 <taku.enginner@gmail.com>
2025-03-03 18:16:39 +0900

    fix:APIコールの例外処理対応

--- app/controllers/musixmatch_controller.rb
+++ app/controllers/musixmatch_controller.rb
@@ -25,6 +25,8 @@ class MusixmatchController < ApplicationController
   def fetch_track(api_key, q_track, q_artist)
     track_encoded_uri = URI::DEFAULT_PARSER.escape("https://api.musixmatch.com/ws/1.1/matcher.track.get?apikey=#{api_key}&q_track=#{q_track}&q_artist=#{q_artist}")
     track_response = HTTParty.get(track_encoded_uri)
+    return nil if track_response.body.blank?
+
     parsed_json_track_response = JSON.parse(track_response.body)
     parsed_json_track_response['message']['body']['track'].slice('track_name', 'artist_name')
   end
@@ -32,6 +34,8 @@ class MusixmatchController < ApplicationController
   def fetch_lyrics(api_key, q_track, q_artist)
     lyrics_encoded_uri = URI::DEFAULT_PARSER.escape("https://api.musixmatch.com/ws/1.1/matcher.lyrics.get?apikey=#{api_key}&q_track=#{q_track}&q_artist=#{q_artist}")
     lyrics_response = HTTParty.get(lyrics_encoded_uri)
+    return nil if lyrics_response.body.blank?
+
     parsed_json_lyrics_response = JSON.parse(lyrics_response.body)
     parsed_json_lyrics_response['message']['body']['lyrics']['lyrics_body']
   end
