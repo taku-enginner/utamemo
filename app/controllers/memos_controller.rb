e2c4b5ac7708d562e08df26862493ea0d88a3ed8
taka4655 <taku.enginner@gmail.com>
2025-03-03 22:16:04 +0900

    add:メモ作成画面の歌詞の表示は成功

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -6,6 +6,10 @@ class MemosController < ApplicationController
 
   def show
     @memo = Memo.find(params[:id])
+    api_key = Rails.application.credentials[:musixmatch_api_key]
+    title = @memo[:song_title]
+    artist_name = @memo[:artist_name]
+    @lyrics_result = fetch_lyrics(api_key, title, artist_name)
   end
 
   def create
@@ -14,7 +18,7 @@ class MemosController < ApplicationController
     @memo = Memo.new(memo_params)
     @memo.user_id = current_user.id
     if @memo.save
-      redirect_to memo_path(@memo.id)
+      redirect_to memo_path(@memo, title: @memo.song_title, artist_name: @memo.artist_name)
     else
       render "musixmatch/search", alert: "メモの開始に失敗しました。"
     end
@@ -29,6 +33,17 @@ class MemosController < ApplicationController
   private
 
   def memo_params
-    params.require(:memo).permit(:song_title, :artist_name)
+    params.require(:memo).except(:lyrics).permit(:song_title, :artist_name)
+  end
+
+  def fetch_lyrics(api_key, q_track, q_artist)
+    lyrics_encoded_uri = URI::DEFAULT_PARSER.escape("https://api.musixmatch.com/ws/1.1/matcher.lyrics.get?apikey=#{api_key}&q_track=#{q_track}&q_artist=#{q_artist}")
+    lyrics_response = HTTParty.get(lyrics_encoded_uri)
+    lyrics_response_code_check = JSON.parse(lyrics_response)
+    # puts "lyrics_response: #{JSON.pretty_generate(lyrics_response_code_check)}"
+    return nil if lyrics_response_code_check['message']['header']['status_code'] == 404
+
+    parsed_json_lyrics_response = JSON.parse(lyrics_response)
+    parsed_json_lyrics_response['message']['body']['lyrics']['lyrics_body']
   end
 end
