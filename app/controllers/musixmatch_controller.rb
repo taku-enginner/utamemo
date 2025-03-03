ac99b856fd5b41827a16ff8e482204c4fd07ca2b
taka4655 <taku.enginner@gmail.com>
2025-03-03 18:07:56 +0900

    fix:musixmatch_controllerの修正

--- app/controllers/musixmatch_controller.rb
+++ app/controllers/musixmatch_controller.rb
@@ -5,15 +5,15 @@ require 'json'
 
 class MusixmatchController < ApplicationController
   def search
-    Rails.application.credentials[:musixmatch_api_key]
-    search_params[:title]
-    search_params[:artist_name]
+    api_key = Rails.application.credentials[:musixmatch_api_key]
+    title = search_params[:title]
+    artist_name = search_params[:artist_name]
 
     # トラックデータ取得
-    @track_result = fetch_track
+    @track_result = fetch_track(api_key, title, artist_name)
 
     # 歌詞取得
-    @lyrics_result = fetch_lyrics
+    @lyrics_result = fetch_lyrics(api_key, title, artist_name)
   end
 
   private
@@ -22,14 +22,14 @@ class MusixmatchController < ApplicationController
     params.permit(:title, :artist_name)
   end
 
-  def fetch_track
+  def fetch_track(api_key, q_track, q_artist)
     track_encoded_uri = URI::DEFAULT_PARSER.escape("https://api.musixmatch.com/ws/1.1/matcher.track.get?apikey=#{api_key}&q_track=#{q_track}&q_artist=#{q_artist}")
     track_response = HTTParty.get(track_encoded_uri)
     parsed_json_track_response = JSON.parse(track_response.body)
     parsed_json_track_response['message']['body']['track'].slice('track_name', 'artist_name')
   end
 
-  def fetch_lyrics
+  def fetch_lyrics(api_key, q_track, q_artist)
     lyrics_encoded_uri = URI::DEFAULT_PARSER.escape("https://api.musixmatch.com/ws/1.1/matcher.lyrics.get?apikey=#{api_key}&q_track=#{q_track}&q_artist=#{q_artist}")
     lyrics_response = HTTParty.get(lyrics_encoded_uri)
     parsed_json_lyrics_response = JSON.parse(lyrics_response.body)
