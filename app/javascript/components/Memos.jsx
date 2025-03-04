bfec81b4463788232af10e5bacdbd066d9dc668e
taka4655 <taku.enginner@gmail.com>
2025-03-04 20:22:19 +0900

    add:メモ作成画面のコンポーネント複製機能の追加

--- app/javascript/components/Memos.jsx
+++ app/javascript/components/Memos.jsx
@@ -11,21 +11,28 @@ export default function Memos({ memo_components }) {
     setComponents(memo_components);
   }, [memo_components]);
   
+  const addComponent = () => {
+    const componentId = components.length + 1;
+    setComponents([...components, { x: 100, y: 100, id: componentId, content: `New Component ${componentId}`}])
+  }
   return (
     <>
-      <div className="bg-red-200">
-
-        {components?.map((component) => (
-          <Draggable
-            key={component.id}
-            position={{ x: component.x, y: component.y}}
-            bounds="parent"
-          >
-            <div className="bg-white p-2 border border-gray-300 rounded-md shadow-md">
-              {component.content}
-            </div>
-          </Draggable>
-        ))}
+      <div className="relative">
+        <div className="absolute top-0 left-0 w-full">
+          <button onClick={addComponent} className="btn">コンポーネントを追加</button>
+          {components?.map((component) => (
+            <Draggable
+              key={component.id}
+              position={{ x: component.x, y: component.y}}
+              bouds="parent"
+            >
+              <div className="component bg-white border border-gray-300 rounded-md shadow-md p-2 
+              max-w-xs w-fit whitespace-normal break-words flex items-center justify-center absolute">
+                {component.content}
+              </div>
+            </Draggable>
+            ))}
+        </div>
       </div>
     </>
   );
