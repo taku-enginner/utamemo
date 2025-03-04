ae3d9b6cfb692041a1ea445e7528bca82ed518bc
taka4655 <taku.enginner@gmail.com>
2025-03-04 14:03:08 +0900

    fix:Memoのshowテンプレートでreactを読み込ませる

--- app/javascript/react/features/memos/index.jsx
+++ app/javascript/react/features/memos/index.jsx
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useState, useEfect } from "react";
+import Draggable from "react-draggable";
 
-export default function Memos() {
+export default function Memos(インスタンス変数) {
+  const [components, setComponents] = useState([]);
+  const [selectedComponent, setSelectedComponent] = useState(null);
+  const [componentGroups, setComponentGroups] = useState([]);
+
+  setComponentGroups(インスタンス変数)
+  console.log("componentGroups", componentGroups);
 
   return (
     <>
