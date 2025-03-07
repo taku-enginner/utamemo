59c19dbbaed18197412f3316866bf36e5023356b
taka4655 <taku.enginner@gmail.com>
2025-03-07 14:42:01 +0900

    fix/manifest.jsonの修正、他ユーザーのメモの表示バグ修正、タイトル修正

--- app/javascript/components/OtherMemos.jsx
+++ app/javascript/components/OtherMemos.jsx
@@ -10,13 +10,39 @@ export default function OtherMemos({ memo }) {
           {memo.memo_components?.map((component) => (
             <div
             key={component.id}
-            className="absolute bg-white border border-gray-300 rounded-md shadow-md p-2"
+            className="absolute bg-white rounded-md shadow-md"
             style={{
               left: `${component.x}px`,  // x座標
               top: `${component.y}px`,   // y座標
             }}
             >
-              {component.content}
+              <div className="text-black text-2xl bg-white rounded-lg">
+                    {
+                      // コンポーネントの数字によって画像を変更。三項演算子を使用
+                      component.type==="technique" ? (() => {
+                        let techniqueImage;
+                        switch (component.content) {
+                          case "1":
+                            techniqueImage = <img src="/technique/ビブラート.png" alt="ビブラート" style={{ width: "50px", height: "50px"}}/>
+                            break;
+                          case "2":
+                            techniqueImage = <img src="/technique/しゃくり.png" alt="しゃくり" style={{ width: "50px", height: "50px"}}/>
+                            break;
+                          case "3":
+                            techniqueImage = <img src="/technique/こぶし.png" alt="こぶし" style={{ width: "50px", height: "50px"}}/>
+                            break;
+                          case "4":
+                            techniqueImage = <img src="/technique/フォール.png" alt="フォール" style={{ width: "50px", height: "50px"}}/>
+                            break;
+                          case "5":
+                            techniqueImage = <img src="/technique/ブレス.png" alt="ブレス" style={{ width: "50px", height: "50px"}}/>
+                            break;
+                          default:
+                            techniqueImage =  <div className="bg-red-500 text-white">表示エラーが起きました。</div>
+                          }
+                        return techniqueImage;
+                        })() : component.content}
+                  </div>
             </div>
           ))}
         </div>
