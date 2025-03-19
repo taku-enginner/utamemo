2d4f6fbcea4eeb6d45f565eb86e8662f6dd3ab1a
taka4655 <taku.enginner@gmail.com>
2025-03-19 09:30:05 +0900

    memosコントローラーのindexアクションのテストまで

---
+++ spec/requests/memos_requests_spec.rb
@@ -0,0 +1,14 @@
+require "rails_helper"
+
+RSpec.describe "Memosコントローラーのテスト", type: :request do
+  it "メモ一覧ページが表示される" do
+    get memos_path
+    expect(response.body).to include "新着メモ一覧"
+  end
+  # let(:user) {create(:user)}
+  # describe "ログイン済み" do
+  #   before do
+  #     sign_in user
+  #   end
+  # end
+end
