27d2fc24ff88c97acf660c3113aa5723045f4bc6
taka4655 <taku.enginner@gmail.com>
2025-04-27 15:00:19 +0900

    add:userモデルのモデルスペックを記載

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,22 +3,35 @@
 require 'rails_helper'
 
 RSpec.describe User do
-  context '未登録の状態' do
-    it 'ログインに失敗する' do
+  describe "バリデーションチェック" do
+    context "正常系" do
+      it "設定したすべてのバリデーションが機能している" do
+        user = FactoryBot.build(:user)
+        expect(user).to be_valid
+        expect(user.errors).to be_empty
+      end
     end
 
-    it '登録に成功する' do
-    end
+    context "異常系" do
+      it "emailが空欄" do
+        user = FactoryBot.build(:user, email: "")
+        expect(user).to be_invalid
+      end
 
-    it '登録に失敗する' do
-    end
-  end
+      it "パスワードが空欄" do
+        user = FactoryBot.build(:user, password: "")
+        expect(user).to be_invalid
+      end
+      it "パスワード確認が空欄" do
+        user = FactoryBot.build(:user, password_confirmation: "")
+        expect(user).to be_invalid
+      end
 
-  context '登録済みの状態' do
-    it 'ログインに成功する' do
-    end
-
-    it '登録されていないデータなら、ログインに失敗する' do
+      it "emailが既に登録されている" do
+        user1 = FactoryBot.create(:user)
+        user2 = FactoryBot.build(:user, email: user1.email)
+        expect(user2).to be_invalid
+      end
     end
   end
 end
---
+++ spec/requests/users/sessions_spec.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+
+RSpec.describe 'Session', type: :request do
+  describe 'GET /sessions_new' do
+    pending "add some examples (or delete) #{__FILE__}"
+    # context '未登録の状態' do
+    #   it 'ログインに失敗する' do
+    #   end
+  
+    #   it '登録に成功する' do
+    #   end
+  
+    #   it '登録に失敗する' do
+    #   end
+    # end
+  
+    # context '登録済みの状態' do
+    #   it 'ログインに成功する' do
+    #     # ユーザーを作る
+    #     # ログインする
+    #   end
+  
+    #   it '登録されていないデータなら、ログインに失敗する' do
+    #   end
+    # end
+  end
+end
