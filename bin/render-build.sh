4e9becc3f6675d4837f0c806196bc1ec4f09db3d
taka4655 <taku.enginner@gmail.com>
2025-05-12 10:37:07 +0900

    fix:render-build.shのrakeファイルをコメントアウト

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,6 +4,8 @@ bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
 bundle exec rails db:migrate
-bundle exec rake memo:migrate_artist_and_song_data
+
+# bundle exec rake memo:migrate_artist_and_song_data
+
 # DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:migrate:reset
 # bundle exec rails db:seed
