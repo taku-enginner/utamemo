e228ac779a3912d8debed86099b7925417584ec3
taka4655 <taku.enginner@gmail.com>
2025-05-19 00:38:55 +0900

    fix:lintエラー解消

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,5 +13,5 @@ Rails.application.config.assets.version = '1.0'
 # folder are already added.
 # Rails.application.config.assets.precompile += %w[ admin.js admin.css ]
 
-Rails.application.config.assets.paths << Rails.root.join("public/packs")
+Rails.application.config.assets.paths << Rails.public_path.join('packs')
 Rails.application.config.assets.precompile += %w[application.js]
